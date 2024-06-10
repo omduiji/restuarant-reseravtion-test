9292dfd307c8c265ae1748e8f53ec60a3da6ec47
mohamed emad <memad@road9media.com>
2024-06-11 01:38:03 +0300

    adding dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className="app-text-main">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
